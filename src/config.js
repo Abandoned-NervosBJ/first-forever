8aa4c536ee74a030e251fcc4e66a5aa3d9122714
shooter <shooter.hero@gmail.com>
2018-12-01 18:25:18 +0800

    fromAddress

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 const config = {
   chain: 'https://node.cryptape.com',
   contractAddress: '0x3D91cafAe0460D9cA810bbD03aac89e10A613703',
-  privateKey:'0x452afbe580c3680feaf64adc2755d88c7c796d3e622d4f2645557c09a00623f7',
+  fromAddress: '0x2D785cF68B4aF1D3Cf49f426E09A7bB773BB76c0',
+  privateKey: '0x452afbe580c3680feaf64adc2755d88c7c796d3e622d4f2645557c09a00623f7',
 }
 
 module.exports = config
